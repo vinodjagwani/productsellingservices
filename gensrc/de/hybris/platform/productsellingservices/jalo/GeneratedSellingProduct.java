af58ef8672323a4b0e5c8b281dfeed9e88091ddf
Vinod <vinodjagwani@gmail.com>
2015-09-15 13:25:14 +0200

    Update GeneratedSellingProduct.java

--- gensrc/de/hybris/platform/productsellingservices/jalo/GeneratedSellingProduct.java
+++ gensrc/de/hybris/platform/productsellingservices/jalo/GeneratedSellingProduct.java
@@ -1,20 +1,4 @@
-/*
- * ----------------------------------------------------------------
- * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 18.12.2014 23:42:16                         ---
- * ----------------------------------------------------------------
- *  
- * [y] hybris Platform
- *  
- * Copyright (c) 2000-2011 hybris AG
- * All rights reserved.
- *  
- * This software is the confidential and proprietary information of hybris
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with hybris.
- *  
- */
+
 package de.hybris.platform.productsellingservices.jalo;
 
 import de.hybris.platform.jalo.GenericItem;
